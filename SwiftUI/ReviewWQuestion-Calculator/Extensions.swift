0d9a894a24352975a671cce5ec60126c16452b20
Taekyun Kim <brandon1609@naver.com>
2022-05-28 20:55:27 +0900

    (#1) fix: operator Button Color
    Thanks to skipp

--- SwiftUI/ReviewWQuestion-Calculator/Extensions.swift
+++ SwiftUI/ReviewWQuestion-Calculator/Extensions.swift
@@ -11,7 +11,7 @@ import SwiftUI
 extension Color {
     //Todo : enum으로 변경
     static let digitButton = Color(red: 51/255, green: 51/255, blue: 51/255)
-    static let operatorButton = Color(red: 242/255, green: 136/255, blue: 59/255)
+    static let operatorButton = Color(red: 254/255, green: 159/255, blue: 10/255)
     static let funcButton = Color(red: 165/255, green: 165/255, blue: 165/255)
     static let pressedOperatorButton = Color(red: 255/255, green: 255/255, blue: 255/255)
 }
