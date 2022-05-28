a78dc87c5ae8b3bc09eb75d576c15f880872a949
김상현 <kshy1019@naver.com>
2022-05-28 16:04:45 +0900

    뷰 완성

--- UIKit/ReviewWQuestion-Calculator/Scene/Main/ViewController/MainViewController.swift
+++ UIKit/ReviewWQuestion-Calculator/Scene/Main/ViewController/MainViewController.swift
@@ -6,14 +6,57 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet var ACButton: UIButton!
+    @IBOutlet var plusMinusButton: UIButton!
+    @IBOutlet var percentButton: UIButton!
+    @IBOutlet var divideButton: UIButton!
+    @IBOutlet var multiplyButton: UIButton!
+    @IBOutlet var minusButton: UIButton!
+    @IBOutlet var plusButton: UIButton!
+    @IBOutlet var equalButton: UIButton!
+    @IBOutlet var commaButton: UIButton!
+    @IBOutlet var num0Button: UIButton!
+    @IBOutlet var num1Button: UIButton!
+    @IBOutlet var num2Button: UIButton!
+    @IBOutlet var num3Button: UIButton!
+    @IBOutlet var num4Button: UIButton!
+    @IBOutlet var num5Button: UIButton!
+    @IBOutlet var num6Button: UIButton!
+    @IBOutlet var num7Button: UIButton!
+    @IBOutlet var num8Button: UIButton!
+    @IBOutlet var num9Button: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
     }
     
     func setUI() {
+        setButtonCornerRadius()
         
+        func setButtonCornerRadius() {
+            ACButton.cornerRadius = ACButton.layer.frame.width * 0.5
+            plusMinusButton.cornerRadius = plusMinusButton.layer.frame.width * 0.5
+            percentButton.cornerRadius = percentButton.layer.frame.width * 0.5
+            divideButton.cornerRadius = divideButton.layer.frame.width * 0.5
+            multiplyButton.cornerRadius = multiplyButton.layer.frame.width * 0.5
+            minusButton.cornerRadius = minusButton.layer.frame.width * 0.5
+            plusButton.cornerRadius = plusButton.layer.frame.width * 0.5
+            equalButton.cornerRadius = equalButton.layer.frame.width * 0.5
+            commaButton.cornerRadius = commaButton.layer.frame.width * 0.5
+            num0Button.cornerRadius = num0Button.layer.frame.width * 0.25
+            num1Button.cornerRadius = num1Button.layer.frame.width * 0.5
+            num2Button.cornerRadius = num2Button.layer.frame.width * 0.5
+            num3Button.cornerRadius = num3Button.layer.frame.width * 0.5
+            num4Button.cornerRadius = num4Button.layer.frame.width * 0.5
+            num5Button.cornerRadius = num5Button.layer.frame.width * 0.5
+            num6Button.cornerRadius = num6Button.layer.frame.width * 0.5
+            num7Button.cornerRadius = num7Button.layer.frame.width * 0.5
+            num8Button.cornerRadius = num8Button.layer.frame.width * 0.5
+            num9Button.cornerRadius = num9Button.layer.frame.width * 0.5
+        }
     }
 }
 
