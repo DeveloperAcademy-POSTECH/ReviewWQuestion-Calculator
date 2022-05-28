6f5fad0a354d9e38bba3c2330a87fcaa2de5ea8c
jayden000106 <cars0106@icloud.com>
2022-05-29 03:20:20 +0900

    feat: #1 숫자 키패드, 사칙연산 키패드, 부가 기능 키패드 만들기

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -6,9 +6,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let keypads: [[String]] = [
+        ["AC", "P&M", "%", "/"],
+        ["7", "8", "9", "*"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 0) {
+                ForEach(keypads, id: \.self) { keypads in
+                    HStack(spacing: 0) {
+                        ForEach(keypads, id: \.self) { keypad in
+                            Button(action: {
+                                
+                            }, label: {
+                                if keypad == "0" {
+                                    ZStack(alignment: .leading) {
+                                        RoundedRectangle(cornerRadius: 40)
+                                            .fill(.gray)
+                                            .frame(width: 174,height: 80)
+                                        Text(keypad)
+                                            .foregroundColor(.white)
+                                            .font(.title)
+                                            .padding(.leading, 32)
+                                    }
+                                }
+                                else {
+                                    ZStack {
+                                        Circle()
+                                            .fill(.gray)
+                                            .frame(width: 80, height: 80)
+                                        Text(keypad)
+                                            .foregroundColor(.white)
+                                            .font(.title)
+                                    }
+                                }
+                            })
+                            .padding(7)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 17)
+        }
     }
 }
 
