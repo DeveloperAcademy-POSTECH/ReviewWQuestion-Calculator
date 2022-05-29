35cb998c65d0ae4097be7c1fbe1a0103be561dbf
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-05-30 01:43:59 +0900

    [FEAT] 버튼 구현
    버튼 구현

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -6,8 +6,93 @@
 import SwiftUI
 import Foundation
 
+enum Buttons : String{
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case plus = "+"
+    case minus = "ㅡ"
+    case equal = "="
+    case divide = "/"
+    case multiple = "X"
+    case dot = "."
+    case percentage = "%"
+    case clear = "AC"
+    case opposite = "+/-"
+    
+    var bgColor : Color {
+        switch self{
+        case .clear,.opposite,.percentage:
+            return Color.gray
+        case .divide,.multiple,.minus,.plus,.equal:
+            return Color.orange
+        default:
+            return Color("btncolor")       }
+    }
+    
+    var foreColor : Color {
+        switch self{
+        case .clear,.opposite,.percentage:
+            return Color.black
+        default:
+            return Color.white        }
+    
+    }
+    
+    var btnwidth : CGFloat {
+        switch self{
+        case .zero:
+            return UIScreen.main.bounds.width / 32 * 14 - 30
+        case .clear,.opposite:
+            return UIScreen.main.bounds.width / 32 * 7 - 15
+        case .percentage :
+            return UIScreen.main.bounds.width / 32 * 7 - 25
+        case .divide :
+            return UIScreen.main.bounds.width / 32 * 7 - 35
+        case .minus :
+            return UIScreen.main.bounds.width / 32 * 7 - 25
+        default:
+            return UIScreen.main.bounds.width / 32 * 7 - 30}
+    
+    }
+    var btnheight : CGFloat { UIScreen.main.bounds.width / 32 * 7}
+    var zeropadding : CGFloat {
+        switch self{
+        case .zero:
+            return 6.0
+        default:
+            return 2.0 }
+    
+    }
+    var leadingpadding : CGFloat {
+    switch self{
+    case .clear,.opposite :
+        return 15
+    case .percentage:
+        return 25
+    case .divide :
+        return 35
+    case .minus :
+        return 25
+    default :
+        return 30
+        }
+    }
+}
+
+
+
 struct ContentView: View {
-    @State var output:Double = 50000.0
+    @State var outPut:Double = 0.0
+    let buttons : [[Buttons]] = [[.clear,.opposite,.percentage,.divide],[.seven,.eight,.nine,.multiple],[.four,.five,.six,.minus],[.one,.two,.three,.plus],[.zero,.dot,.equal]]
+    
     let numberFormatter = NumberFormatter()
     init(){
         numberFormatter.numberStyle = .decimal
@@ -21,9 +106,10 @@ struct ContentView: View {
             Color.black
                 .ignoresSafeArea()
         VStack{
+            Spacer()
             HStack{
                 Spacer()
-                Text(numberFormatter.string(from: output as NSNumber)!)
+                Text(numberFormatter.string(from: outPut as NSNumber)!)
                     .font(.system(size: 100))
                     .minimumScaleFactor(0.01)
                     .padding()
@@ -31,13 +117,28 @@ struct ContentView: View {
                     .lineLimit(1)
                     .allowsTightening(true)
             }
-            
-            Button {
-                output += 100000000
-                
-            } label: {
-                Text("1")
-                    .foregroundColor(.white)
+            ForEach(buttons,id: \.self){row in
+                HStack{
+                    ForEach(row,id:\.self){btn in
+                        Button {
+                            print(btn.rawValue)
+                        } label: {
+                            Text(btn.rawValue)
+                                .font(.system(size: 40, weight: .bold))
+                                .frame(width: btn.btnwidth, height: btn.btnheight, alignment: .leading)
+                            
+                        }
+                        
+                        .foregroundColor(btn.foreColor)
+                        .padding(.leading,btn.leadingpadding)
+                        .background(btn.bgColor)
+                        .cornerRadius(100)
+                        .padding(.horizontal, btn.zeropadding)
+                        .padding(.vertical, 3.0)
+                        
+                }
+                }
+                .padding(.horizontal, 6)
             }
             
 
