8516b30d3d5ca6d9e9bbe43a03c048bfc8f38f04
terry-koo <xpflxhfl94@naver.com>
2022-05-30 22:56:40 +0900

    #2 실제 계산기 모양으로 버튼 레이아웃 구현

--- SwiftUI/ReviewWQuestion-Calculator/View/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/View/ContentView.swift
@@ -2,13 +2,53 @@
 //  ContentView.swift
 //  ReviewWQuestion-Calculator
 //
+
 import SwiftUI
 
 struct ContentView: View {
+    var data = CalcButton.calcButtonData
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
-        Text("hello")
-    }
-}
+        VStack {
+            HStack {
+                Text("0")
+                    .font(.system(size: 70))
+            }
+            .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height*0.3, alignment: .bottomTrailing)
+            
+            LazyVGrid(columns: columns, spacing: 10){
+                ForEach(data, id: \.id) { i in
+                    Button(action: {
+                        
+                    }, label: {
+                        CircleButtonView(backgroundColor: i.backgroundColor, text: i.text, textColor: i.textColor)
+                    })
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height*0.6)
+            Spacer()
+        } // VStack`
+        .preferredColorScheme(.dark)
+        .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10 ))
+    } // body
+} // ContentView
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
