ba9f9af1435f9dc88e5140e9fa23e261767ddf65
sunshiningsoo <yenkee1219@gmail.com>
2022-05-30 15:40:57 +0900

    [#1] 숫자 및 연산자 UI 완료
    0은 2개의 버튼이 되었지만, 여유 있을때 고쳐보기

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -5,10 +5,28 @@
 
 import SwiftUI
 
+let columns:[GridItem] = [GridItem(), GridItem(), GridItem(), GridItem()]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+             Color(.black)
+            VStack {
+                HStack{
+                    Spacer()
+                    Text("0")
+                        .foregroundColor(.white)
+                        .font(.system(size: 80))
+                        .padding()
+                }
+                LazyVGrid(columns: columns){
+                     ForEach(allButtonsArray){ item in
+                         ButtonUIView(buttonModel:item)
+                     }
+                }
+            }
+         }
+         .ignoresSafeArea()
     }
 }
 
