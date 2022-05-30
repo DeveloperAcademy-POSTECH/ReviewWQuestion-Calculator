767f10e1f3dcc0d3ab504ede93cee9391095c44a
terry-koo <xpflxhfl94@naver.com>
2022-05-30 23:02:20 +0900

    #5 버튼 함수 기본 구현

--- SwiftUI/ReviewWQuestion-Calculator/Model/ButtonModel.swift
+++ SwiftUI/ReviewWQuestion-Calculator/Model/ButtonModel.swift
@@ -78,3 +78,73 @@ extension CalcButton {
         CalcButton(text: "=", backgroundColor: Color.buttonOrange, textColor: Color.textWhite)
     ]
 }
+
+
+
+extension CalcButton {
+    mutating func clear(){
+        while !stack.isEmpty() {
+            stack.pop()
+        }
+    }
+    
+    mutating func equal(){
+        
+    }
+    
+    mutating func divide(){
+        stack.push("÷")
+    }
+    
+    mutating func time(){
+        stack.push("×")
+    }
+    
+    mutating func plus(){
+        stack.push("÷")
+    }
+    
+    mutating func minus(){
+        stack.push("−")
+    }
+    
+    mutating func zero(){
+        stack.push("0")
+    }
+
+    mutating func one(){
+        stack.push("1")
+    }
+    
+    mutating func two(){
+        stack.push("2")
+    }
+    
+    mutating func three(){
+        stack.push("3")
+    }
+    
+    mutating func four(){
+        stack.push("4")
+    }
+    
+    mutating func five(){
+        stack.push("5")
+    }
+    
+    mutating func six(){
+        stack.push("6")
+    }
+    
+    mutating func seven(){
+        stack.push("7")
+    }
+    
+    mutating func eight(){
+        stack.push("8")
+    }
+    
+    mutating func nine(){
+        stack.push("9")
+    }
+}
