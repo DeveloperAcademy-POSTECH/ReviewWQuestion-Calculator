e33c1737f034ba6d7447cd5c801e64e4afeba6e0
sunshiningsoo <yenkee1219@gmail.com>
2022-05-30 14:36:59 +0900

    [#1] 숫자 및 연산자 UI 완료!
    0이 RoundedRectangle로 안되었고 2개의 버튼이 된 것은 추후에 변경할 수 있을때 하자!

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -5,10 +5,22 @@
 
 import SwiftUI
 
+
+// 색상마다의 클래스 기능을 주게해서 반복을 줄이는 방법
+
+let columns:[GridItem] = [GridItem(), GridItem(), GridItem(), GridItem()]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.black)
+            LazyVGrid(columns: columns){
+                ForEach(allButtonsArray){ item in
+                    ButtonUIView(buttonModel:item)
+                }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
---
+++ SwiftUI/ReviewWQuestion-Calculator/Views/ButtonUIView.swift
@@ -0,0 +1,39 @@
+//
+//  ButtonUIView.swift
+//  ReviewWQuestion-Calculator
+//
+//  Created by 박성수 on 2022/05/29.
+//
+
+import SwiftUI
+
+var buttonWidth:CGFloat = 85
+var buttonHeight:CGFloat = 85
+
+struct ButtonUIView: View {
+    var buttonModel:ButtonModel
+    
+    var body: some View {
+        ZStack {
+            Button(action: {
+                
+            }, label: {
+                Circle().foregroundColor(buttonModel.color)
+            })
+            .frame(width:buttonWidth, height: buttonHeight)
+            Text(buttonModel.name)
+                .foregroundColor(buttonModel.function == "function" ? .black : .white)
+                .font(.system(size: 40))
+        }
+    }
+}
+
+struct ButtonUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ButtonUIView(buttonModel: ButtonModel(name: "AC", function: "function", color: Color(.lightGray)))
+            ButtonUIView(buttonModel: ButtonModel(name: "+", function: "operator", color: Color(.systemOrange)))
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
