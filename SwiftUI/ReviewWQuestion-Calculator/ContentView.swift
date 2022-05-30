0a8f1f25ffc5d6768c72298332de467074744993
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-05-31 02:06:18 +0900

    #9 FEAT 누른 일반 버튼 text에 표시

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -43,7 +43,7 @@ enum Buttons : String{
             return Color.black
         default:
             return Color.white        }
-    
+        
     }
     
     var btnwidth : CGFloat {
@@ -60,7 +60,7 @@ enum Buttons : String{
             return UIScreen.main.bounds.width / 32 * 7 - 25
         default:
             return UIScreen.main.bounds.width / 32 * 7 - 30}
-    
+        
     }
     var btnheight : CGFloat { UIScreen.main.bounds.width / 32 * 7}
     var zeropadding : CGFloat {
@@ -69,20 +69,20 @@ enum Buttons : String{
             return 6.0
         default:
             return 2.0 }
-    
+        
     }
     var leadingpadding : CGFloat {
-    switch self{
-    case .clear,.opposite :
-        return 15
-    case .percentage:
-        return 25
-    case .divide :
-        return 35
-    case .minus :
-        return 25
-    default :
-        return 30
+        switch self{
+        case .clear,.opposite :
+            return 15
+        case .percentage:
+            return 25
+        case .divide :
+            return 35
+        case .minus :
+            return 25
+        default :
+            return 30
         }
     }
 }
@@ -90,68 +90,137 @@ enum Buttons : String{
 
 
 struct ContentView: View {
-    @State var outPut:Double = 0.0
+    @State var outPut:Double = 0
+    @State var outputString  : String = "0"
+    @State var isTouched = false
+    @State var isDot = false
+    @State var isonDot = false
     let buttons : [[Buttons]] = [[.clear,.opposite,.percentage,.divide],[.seven,.eight,.nine,.multiple],[.four,.five,.six,.minus],[.one,.two,.three,.plus],[.zero,.dot,.equal]]
     
-    let numberFormatter = NumberFormatter()
+    
+    let numberFormatter1 = NumberFormatter()
     init(){
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.maximumSignificantDigits = 9
-//        numberFormatter.maximumFractionDigits = 9
-        numberFormatter.maximumIntegerDigits = 9
+        numberFormatter1.maximumFractionDigits = 9
     }
     
     var body: some View {
         ZStack{
             Color.black
                 .ignoresSafeArea()
-        VStack{
-            Spacer()
-            HStack{
+            VStack{
                 Spacer()
-                Text(numberFormatter.string(from: outPut as NSNumber)!)
-                    .font(.system(size: 100))
-                    .minimumScaleFactor(0.01)
-                    .padding()
-                    .foregroundColor(.white)
-                    .lineLimit(1)
-                    .allowsTightening(true)
-            }
-            ForEach(buttons,id: \.self){row in
                 HStack{
-                    ForEach(row,id:\.self){btn in
-                        Button {
-                            print(btn.rawValue)
-                        } label: {
-                            Text(btn.rawValue)
-                                .font(.system(size: 40, weight: .bold))
-                                .frame(width: btn.btnwidth, height: btn.btnheight, alignment: .leading)
+                    Spacer()
+                    Text(formatDouble(str: outputString))
+                        .font(.system(size: 100))
+                        .minimumScaleFactor(0.01)
+                        .padding()
+                        .foregroundColor(.white)
+                        .lineLimit(1)
+                        .allowsTightening(true)
+                }
+                ForEach(buttons,id: \.self){row in
+                    HStack{
+                        ForEach(row,id:\.self){btn in
+                            Button {
+                                btnAction(button: btn)
+                            } label: {
+                                Text(btn.rawValue)
+                                    .font(.system(size: 40, weight: .bold))
+                                    .frame(width: btn.btnwidth, height: btn.btnheight, alignment: .leading)
+                            }
+                            .foregroundColor(btn.foreColor)
+                            .padding(.leading,btn.leadingpadding)
+                            .background(btn.bgColor)
+                            .cornerRadius(100)
+                            .padding(.horizontal, btn.zeropadding)
+                            .padding(.vertical, 3.0)
                             
                         }
-                        
-                        .foregroundColor(btn.foreColor)
-                        .padding(.leading,btn.leadingpadding)
-                        .background(btn.bgColor)
-                        .cornerRadius(100)
-                        .padding(.horizontal, btn.zeropadding)
-                        .padding(.vertical, 3.0)
-                        
+                    }
+                    .padding(.horizontal, 6)
                 }
-                }
-                .padding(.horizontal, 6)
+                
+                
             }
             
-
         }
-        
-    }
         .onAppear {
             //세로모드 고정
-                    UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
-                    AppDelegate.orientationLock = .portrait
-                }.onDisappear {
-                    AppDelegate.orientationLock = .all
+            UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+            AppDelegate.orientationLock = .portrait
+        }.onDisappear {
+            AppDelegate.orientationLock = .all
+        }
+    }
+    func formatDouble(str: String)->String{
+        let number = Double(str)!
+        
+        if ((abs(number) > 999999999 || abs(number)<0.00000001) && number != 0 ){
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .scientific
+            formatter.positiveFormat = "0.#####E0"
+            formatter.exponentSymbol = "e"
+            
+            return formatter.string(from: number as NSNumber)!
+        }
+        else if(number<1000){
+            let numberFormatter = NumberFormatter()
+            
+            return str
+        }
+        else{
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            numberFormatter.maximumSignificantDigits = 9
+            //numberFormatter.maximumFractionDigits = 9
+            numberFormatter.maximumIntegerDigits = 9
+            return numberFormatter.string(from: number as NSNumber)!
+        }
+    }
+    
+    
+    func btnAction(button: Buttons){
+        switch button{
+        case .divide:
+            break
+        case .minus:
+            break
+        case .plus:
+            break
+        case .equal:
+            break
+        case .multiple:
+            break
+        case .percentage:
+            break
+        case .opposite:
+            break
+        case .clear:
+            outputString = "0"
+            isDot = false
+        case .dot:
+            if !isDot{
+                isDot = true
+                outputString.append(contentsOf: ".")
+            }
+            
+        default:
+            if (outputString == "0") {
+                outputString = button.rawValue
+            }
+            else {
+                if (((abs(Double(outputString + button.rawValue)!.exponent) > 999999999) || (abs(Double(outputString + button.rawValue)!) < 0.00000001)) && (outputString != "0")){
+                    print(outputString)
+                    break
                 }
+                else{
+                    outputString.append(contentsOf: button.rawValue)
+                   
+                }
+                
+            }
+        }
     }
 }
 
