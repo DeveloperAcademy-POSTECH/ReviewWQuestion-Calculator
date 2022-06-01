fdbd2fe8c59cf11f05a5e29ecd41a1d6db39f80b
otoolz <otool.hf@gmail.com>
2022-06-01 21:44:17 +0900

    [#3] 추가 세로 고정 모드
    세로 고정 모드 추가
    출처 : https://stackoverflow.com/questions/66037782/swiftui-how-do-i-lock-a-particular-view-in-portrait-mode-whilst-allowing-others

--- SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ContentView.swift
@@ -17,6 +17,11 @@ struct ContentView: View {
                 CalculatorButtonView(vm: vm)
             }
             .preferredColorScheme(.dark)
+        }.onAppear {
+            UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation") // Forcing the rotation to portrait
+            AppDelegate.orientationLock = .portrait // And making sure it stays that way
+        }.onDisappear {
+            AppDelegate.orientationLock = .all // Unlocking the rotation when leaving the view
         }
     }
 }
