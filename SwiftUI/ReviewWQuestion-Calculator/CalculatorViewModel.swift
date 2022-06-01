d1ee66e82bf4beecf7d7cdd34c66b4d7f398c787
otoolz <otool.hf@gmail.com>
2022-06-01 16:29:32 +0900

    [#5] 추가 더하기 연산 기능
    + 버튼 연산
    = 버튼 연산

--- SwiftUI/ReviewWQuestion-Calculator/CalculatorViewModel.swift
+++ SwiftUI/ReviewWQuestion-Calculator/CalculatorViewModel.swift
@@ -11,10 +11,10 @@ class CalculatorViewModel: ObservableObject {
     @Published var text: String = "0"
     
     var operands: [Double] = []
-    var resultNumber: Double = 0
+    var tempOperand: Double = 0
     var operations: [String] = []
+    var tempOperation = ""
     var isOperation = false
-    // operation Flag
     // display Flag
     
     func input(button: CalculatorButtonManager) {
@@ -32,15 +32,16 @@ class CalculatorViewModel: ObservableObject {
             addDot()
         case .minus, .plus, .division, .multiplication:
             operate(operation: button.label)
-        case .equal: // result
-            return
+        case .equal:
+            inputEqual()
         }
     }
     
-    func addToText(number: String) {
+    private func addToText(number: String) {
         if isOperation {
             isOperation = false
-            operands.append(Double(text) ?? 0)
+            operands.append(tempOperand)
+            operations.append(tempOperation)
             text = "0"
         }
         if text == "0" {
@@ -50,10 +51,11 @@ class CalculatorViewModel: ObservableObject {
         }
     }
     
-    func addDot() {
+    private func addDot() {
         if isOperation {
             isOperation = false
-            operands.append(Double(text) ?? 0)
+            operands.append(tempOperand)
+            operations.append(tempOperation)
             text = "0."
         }
         if text.contains(".") {
@@ -63,15 +65,33 @@ class CalculatorViewModel: ObservableObject {
         }
     }
     
-    func operate(operation: String) {
+    private func operate(operation: String) {
         //
         //이전 연산에 대한 처리(처리할 것이 있는가)
         //  있다면(operands에 숫자가 있고 operations에 연산기호가 있다)
         //      현재 text를 temp operand에 넣고 operands 팝한거랑 오퍼레이션 팝한거 연산
         //  없다면 temp operand에 현재 text만 넣기
-        
-        if operation == "plus" {
-            isOperation = true
+        isOperation = true
+        switch operation {
+        case "plus" :
+            tempOperation = operation //plus, minus 이전 연산은 무조건 처리
+            tempOperand = Double(text)!
+            // mul, div 이전 연
+        default : return
         }
+    }
+    
+    private func inputEqual() {
+        var result = Double(text)!
+        while !operations.isEmpty {
+            tempOperand = operands.popLast()!
+            tempOperation = operations.popLast()!
+            
+            if tempOperation == "plus" {
+                result += tempOperand
+            }
+        }
+        
+        text = String(result)
     }
 }
