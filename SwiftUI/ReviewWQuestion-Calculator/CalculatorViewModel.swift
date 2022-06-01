af0d2e6758e82f7f034f26eb0ccdced681f0f4c3
otoolz <otool.hf@gmail.com>
2022-06-01 14:18:02 +0900

    [#4] 추가 dot 버튼 입력
    AddDot() 함수 추가
    
    덧셈 구현중..........

--- SwiftUI/ReviewWQuestion-Calculator/CalculatorViewModel.swift
+++ SwiftUI/ReviewWQuestion-Calculator/CalculatorViewModel.swift
@@ -10,10 +10,10 @@ import SwiftUI
 class CalculatorViewModel: ObservableObject {
     @Published var text: String = "0"
     
-    var tempOperand: Double = 0
-    var tempOperation: String = ""
-    var operandStack: [Double] = []
-    var operationStack: [String] = []
+    var operands: [Double] = []
+    var resultNumber: Double = 0
+    var operations: [String] = []
+    var isOperation = false
     // operation Flag
     // display Flag
     
@@ -21,15 +21,15 @@ class CalculatorViewModel: ObservableObject {
         switch button {
         case .allClear: // delete top
             text = "0"
-        case .reverse: // not implement
+        case .reverse: // no
             return
-        case .percent: // not implement
+        case .percent: // no
             return
         case .one, .two, .three, .four, .five,
                 .six, .seven, .eight, .nine, .zero, .zero2:
             addToText(number: button.label)
         case .dot:  // dot
-            return
+            addDot()
         case .minus, .plus, .division, .multiplication:
             operate(operation: button.label)
         case .equal: // result
@@ -38,20 +38,40 @@ class CalculatorViewModel: ObservableObject {
     }
     
     func addToText(number: String) {
+        if isOperation {
+            isOperation = false
+            operands.append(Double(text) ?? 0)
+            text = "0"
+        }
         if text == "0" {
             text = number
         } else {
             text += number
         }
-        tempOperand = Double(text) ?? 0
-        print(tempOperand)
+    }
+    
+    func addDot() {
+        if isOperation {
+            isOperation = false
+            operands.append(Double(text) ?? 0)
+            text = "0."
+        }
+        if text.contains(".") {
+            
+        } else {
+            text += "."
+        }
     }
     
     func operate(operation: String) {
-        operandStack.append(Double(text) ?? 0)
+        //
+        //이전 연산에 대한 처리(처리할 것이 있는가)
+        //  있다면(operands에 숫자가 있고 operations에 연산기호가 있다)
+        //      현재 text를 temp operand에 넣고 operands 팝한거랑 오퍼레이션 팝한거 연산
+        //  없다면 temp operand에 현재 text만 넣기
+        
         if operation == "plus" {
-            tempOperand = Double(text) ?? 0
-            
+            isOperation = true
         }
     }
 }
