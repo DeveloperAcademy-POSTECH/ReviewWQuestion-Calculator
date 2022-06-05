006799f860223f5ced2b4c8f85b02deb0d440913
Taekyun Kim <brandon1609@naver.com>
2022-06-05 16:53:46 +0900

    feat: 세로 모드 고정

--- SwiftUI/ReviewWQuestion-Calculator/ReviewWQuestion_CalculatorApp.swift
+++ SwiftUI/ReviewWQuestion-Calculator/ReviewWQuestion_CalculatorApp.swift
@@ -7,9 +7,18 @@ import SwiftUI
 
 @main
 struct CalculatorApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainView().background(.black)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.all
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
